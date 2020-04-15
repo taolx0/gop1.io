b4d5690f7995d309cf16e40fb3cbd5c3788ca957
TaoLongxiangA <63177450+TaoLongxiangA@users.noreply.github.com>
2020-04-15 11:23:52 +0800

    taolongxiang

---
+++ ch5/html/html.go
@@ -0,0 +1,23 @@
+package html
+
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+type Attribute struct {
+	Key, Val string
+}
