8737733ff4e3c95805bb68779a9b1cde88e96d13
TaoLongxiangA <63177450+TaoLongxiangA@users.noreply.github.com>
2020-04-18 16:03:00 +0800

    taolongxiang

--- src/ch6/geometry/main.go
+++ src/ch6/geometry/main.go
@@ -1,4 +1,4 @@
-package main
+package geometry
 
 import (
 	"fmt"
@@ -7,15 +7,35 @@ import (
 
 type Point struct{ X, Y float64 }
 
+type Path []Point
+
 func main() {
 	p := Point{1, 2}
 	q := Point{4, 6}
 	fmt.Println(Distance(p, q))
 	fmt.Println(p.Diatance(q))
+
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+	fmt.Println(perim.Distance())
 }
 
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Diatance(path[i])
+		}
+	}
+	return sum
 }
 
 func (p Point) Diatance(q Point) float64 {
