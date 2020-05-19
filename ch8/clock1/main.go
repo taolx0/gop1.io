d82b9b435b346c4aaa990eb3b399eae167cd3e62
TaoLongxiangA <63177450+TaoLongxiangA@users.noreply.github.com>
2020-05-19 11:52:21 +0800

    initial commit

---
+++ ch8/clock1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+		handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	_, err := io.WriteString(c, time.Now().Format("16:01:03\n"))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	time.Sleep(1 * time.Second)
+}
