36e7319775feeaf7d4cb4a94b98e0e2c852717df
taolx <taolx25@foxmail.com>
2020-08-31 10:15:51 +0800

    initial commit

--- test.go
+++ test.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	s := "hello"
+	//s1 := make([]byte, 5, 10)
+	slice := []byte{'h', 'e', 'l', 'l', 'o'}
+	fmt.Println(s[:3], reflect.TypeOf(s[:3]))
+	fmt.Println(string(slice[:3]), reflect.TypeOf(slice[:3]))
+
+	for i := range s {
+		fmt.Println(i)
+	}
 }
