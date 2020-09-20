4b4172f39552a0892501c2ae8820e8e9f55c3b28
taolx <taolx25@foxmail.com>
2020-09-20 10:58:42 +0800

    initial commit

--- ch7/bytecounter/main.go
+++ ch7/bytecounter/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p))
+	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("building"))
+	fmt.Println(c)
+
+	c = 0
+	fmt.Fprintf(&c, "%s", "name")
+	fmt.Println(c)
 }
