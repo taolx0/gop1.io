07b48fe9ddd00fb0a2a43a1d665db304b4267ec7
taolx <taolx25@foxmail.com>
2020-09-30 19:53:06 +0800

    initial commit

--- ch4/append/main.go
+++ ch4/append/main.go
@@ -13,15 +13,15 @@ func main() {
 
 func appendInt(x []int, y int) []int {
 	var z []int
-	zlen := len(x) + 1
-	if zlen <= cap(x) {
-		z = x[:zlen] //append slice's content
+	zLen := len(x) + 1
+	if zLen <= cap(x) {
+		z = x[:zLen]
 	} else {
-		zcap := zlen
-		if zcap < 2*len(x) {
-			zcap = 2 * len(x)
+		zCap := zLen
+		if zCap < 2*len(x) {
+			zCap = 2 * len(x)
 		}
-		z = make([]int, zlen, zcap)
+		z = make([]int, zLen, zCap)
 		copy(z, x)
 	}
 	z[len(x)] = y
